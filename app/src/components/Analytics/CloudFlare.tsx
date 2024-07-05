bbd5e7b0a06f363e243b2618e1c79fd16e6c1a32
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2024-07-04 18:54:05 -0700

    Adding cloudflare analytics as its free (#18)

---
+++ app/src/components/Analytics/CloudFlare.tsx
@@ -0,0 +1,16 @@
+// Documentation: https://developers.cloudflare.com/analytics/web-analytics/
+const CloudFlareAnalytics = () => {
+    return (
+        <div>
+            {/* Default CloudFlare code for Digital Madness https://digital-madness.in/ */}
+            <script
+                defer
+                src="https://static.cloudflareinsights.com/beacon.min.js"
+                data-cf-beacon='{"token": "64279c3841a2410d93fe95af2c0c3b1c"}'
+            ></script>
+            {/* End of CloudFlare Code */}
+        </div>
+    )
+}
+
+export default CloudFlareAnalytics
--- app/src/components/Analytics/SiteAnalytics.tsx
+++ app/src/components/Analytics/SiteAnalytics.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
+import CloudFlareAnalytics from './CloudFlare'
 import StatCounter from './StatCounter'
 
 export default function SiteAnalytics(): React.ReactElement {
     return (
         <>
+            <CloudFlareAnalytics />
             <StatCounter />
         </>
     )
